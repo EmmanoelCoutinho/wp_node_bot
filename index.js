1832dc28f0429e32ccddbcd28fea2e8ed52f5800
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2020-10-24 12:30:56 -0300

    frist commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Supports ES6
+// import { create, Whatsapp } from 'sulla';
+const sulla = require('sulla');
+
+sulla.create().then((client) => start(client));
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Hi') {
+      client.sendText(message.from, '👋 Hello from sulla!');
+    }
+  });
+}
